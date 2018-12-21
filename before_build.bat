c1c309604d6c019a5f2bde55f6f2394227a24fd5
Michael Tryby <tryby.michael@epa.gov>
2018-12-21 12:34:29 -0500

    Reorganizing package layout

---
+++ before_build.bat
@@ -0,0 +1,34 @@
+::
+::  before_build.bat - Prepares for swmm toolkit and output module builds
+::
+::  Date Created: 12/21/2018
+::
+::  Author: Michael E. Tryby
+::          US EPA - ORD/NRMRL
+::
+
+set PROJECT_PATH=%~1
+
+set TOOLKIT_PATH=\swmm_python\toolkit\swmm\toolkit
+set OUTPUT_PATH=\swmm_python\output\swmm\output
+
+
+mkdir buildlib
+cd buildlib
+git clone --branch=feature-wrapper https://github.com/michaeltryby/Stormwater-Management-Model.git swmm
+cd swmm 
+
+
+mkdir buildprod
+cd buildprod
+cmake -G"Visual Studio 14 2015 Win64" -DBUILD_TESTS=0 ..
+cmake --build . --config Release
+
+
+copy /Y .\bin\Release\swmm5.dll  %PROJECT_PATH%\%TOOLKIT_PATH%
+copy /Y .\lib\Release\swmm5.lib  %PROJECT_PATH%\%TOOLKIT_PATH%
+copy /Y ..\include\*.h  %PROJECT_PATH%\%TOOLKIT_PATH%
+
+copy /Y .\bin\Release\swmm-output.dll  %PROJECT_PATH%\%OUTPUT_PATH%
+copy /Y .\lib\Release\swmm-output.lib  %PROJECT_PATH%\%OUTPUT_PATH%
+copy /Y ..\tools\swmm-output\include\*.h  %PROJECT_PATH%\%OUTPUT_PATH%
--- swmm_python/toolkit/tests/test_toolkit.py
+++ swmm_python/toolkit/tests/test_toolkit.py
@@ -1,16 +1,20 @@
 #
 #  test_toolkit.py
-#   
+#
 #  Created:    8/8/2018
 #  Author:     Michael E. Tryby
 #              US EPA - ORD/NRMRL
-#  
+#
+import os
 
 import pytest
 
 from swmm.toolkit import toolkit as smtk
 
-from data import INPUT_FILE_EXAMPLE_1, REPORT_FILE_TEST, OUTPUT_FILE_TEST
+DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
+INPUT_FILE_EXAMPLE_1 = os.path.join(DATA_PATH, 'Example1.inp')
+REPORT_FILE_TEST = os.path.join(DATA_PATH, 'test.rpt')
+OUTPUT_FILE_TEST = os.path.join(DATA_PATH, 'test.out')
 
 
 def test_allocfree():
@@ -19,44 +23,43 @@ def test_allocfree():
     _handle = smtk.free_project(_handle)
     assert(_handle == None)
 
- 
+
 def test_run():
     _handle = smtk.alloc_project()
     smtk.run(_handle, INPUT_FILE_EXAMPLE_1, REPORT_FILE_TEST, OUTPUT_FILE_TEST)
     smtk.free_project(_handle)
-    
-    
+
+
 def test_openclose():
     _handle = smtk.alloc_project()
     smtk.open(_handle, INPUT_FILE_EXAMPLE_1, REPORT_FILE_TEST, OUTPUT_FILE_TEST)
     smtk.close(_handle)
     smtk.free_project(_handle)
 
-    
+
 @pytest.fixture()
-def handle(request):    
+def handle(request):
     _handle = smtk.alloc_project()
     smtk.open(_handle, INPUT_FILE_EXAMPLE_1, REPORT_FILE_TEST, OUTPUT_FILE_TEST)
-    
+
     def close():
         smtk.close(_handle)
         smtk.free_project(_handle)
-    
-    request.addfinalizer(close)    
-    return _handle    
+
+    request.addfinalizer(close)
+    return _handle
 
 
 def test_step(handle):
-     
+
      smtk.start(handle, 0)
-     
+
      while True:
          time = smtk.step(handle)
-         
+
          if time == 0.:
              break
 
      smtk.end(handle)
-     
-     smtk.report(handle)
 
+     smtk.report(handle)
