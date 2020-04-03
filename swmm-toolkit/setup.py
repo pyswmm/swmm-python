1c04c4ee6babcf054249549e23988bcb98ca74c2
Michael Tryby <tryby.michael@epa.gov>
2020-04-03 12:44:27 -0400

    Work in progress
    Fixes file staging issue

--- swmm-toolkit/setup.py
+++ swmm-toolkit/setup.py
@@ -4,7 +4,7 @@
 # setup.py - Setup script for swmm-toolkit python package
 #
 # Created:    7/2/2018
-# Modified:   2/21/2020
+# Modified:   4/3/2020
 #
 # Author:     Michael E. Tryby
 #             US EPA - ORD/NRMRL
@@ -13,22 +13,36 @@
 #   python setup.py build -- -G"Visual Studio 15 2017 Win64" ..
 #
 
+import platform
 
 from skbuild import setup
 
 
+# Determine platform
+platform_system = platform.system()
+
+
+# Set Platform specific cmake args here
+if platform_system == "Windows":
+    cmake_args = ["-GVisual Studio 14 2015 Win64"]
+
+elif platform_system == "Darwin":
+    cmake_args = ["-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.10"]
+
+else:
+    cmake_args = ["-GNinja"]
+
+
 setup(
-    name = 'swmm-toolkit',
-    version = '0.4.0',
+    name = "swmm-toolkit",
+    version = "0.4.0",
 
-    cmake_args = ['-GVisual Studio 14 2015 Win64'],
+    cmake_args=cmake_args,
 
-    package_dir={'': 'src'},
-    packages=['swmm.toolkit'],
+    package_dir={"": "src"},
+    packages=["swmm.toolkit"],
 
-    package_data = {
-        'swmm.toolkit':['*solver*','*output*', '*.dylib', '*.dll', '*.so'],
-    },
+    include_package_data=True,
 
     zip_safe=False
 )
