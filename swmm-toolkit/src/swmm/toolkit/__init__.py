792471938b32cd8de318a3ba45dd9276c0547d5a
Michael Tryby <tryby.michael@epa.gov>
2020-05-20 17:58:58 -0400

    Fixes path issue on Windows

--- swmm-toolkit/src/swmm/toolkit/__init__.py
+++ swmm-toolkit/src/swmm/toolkit/__init__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 #
-#  __init__.py - SWMM output package
+#  __init__.py - SWMM toolkit package
 #
 #  Created:    Aug 9, 2018
-#  Updated:    May 8, 2020
+#  Updated:    May 20, 2020
 #
 #  Author:     Michael E. Tryby
 #              US EPA - ORD/NRMRL
 #
 
 '''
-A low level pythonic API for the swmm-output dll using SWIG.
+A low level pythonic API for the swmm-output and swmm-output dlls using SWIG.
 '''
 
 
@@ -21,8 +21,22 @@ __credits__ = "Colleen Barr, Sam Hatchett"
 __license__ = "CC0 1.0 Universal"
 
 __version__ = "0.5.0"
-__date__ = "May 8, 2020"
+__date__ = "May 20, 2020"
 
 __maintainer__ = "Michael Tryby"
 __email__ = "tryby.michael@epa.gov"
 __status__  = "Development"
+
+
+import os
+import platform
+
+
+# Adds directory containing swmm libraries to path
+if platform.system() == "Windows":
+    libdir = os.path.join(os.path.dirname(__file__), "../../../../bin")
+
+    if hasattr(os, 'add_dll_directory'):
+        os.add_dll_directory(libdir)
+    else:
+        os.environ["PATH"] = libdir + ";" + os.environ["PATH"]
