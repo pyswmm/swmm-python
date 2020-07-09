18ae6ae73fc9b65aafe6851efa9dd7a197176481
Laurent Courty <lrntct@gmail.com>
2020-07-09 00:20:52 -0500

    setup.py: do not force Windows compiler

--- swmm-toolkit/setup.py
+++ swmm-toolkit/setup.py
@@ -58,7 +58,8 @@ platform_system = platform.system()
 
 # Set Platform specific cmake args here
 if platform_system == "Windows":
-    cmake_args = ["-GVisual Studio 14 2015 Win64"]
+    pass
+    # cmake_args = ["-GVisual Studio 14 2015 Win64"]
 
 elif platform_system == "Darwin":
     cmake_args = ["-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.10"]
