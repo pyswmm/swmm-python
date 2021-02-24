8944694d6bff7007cea9ff968254db46f9934f6d
Michael Tryby <tryby.michael@epa.gov>
2021-02-24 15:57:40 -0500

    Setting up manylinux build

--- swmm-toolkit/tools/build-wheels.sh
+++ swmm-toolkit/tools/build-wheels.sh
@@ -1,24 +1,62 @@
 #!/bin/bash
 set -e -x
 
+
 # Install a system package required by our library
 sudo yum install -y swig
 
-# Compile wheels
+
+# Setup and build in swmm-toolkit dir
+mkdir -p ./dist
+cd swmm-toolkit
+
+# Build wheels
 for PYBIN in /opt/python/*/bin; do
-    ${PYBIN}/python -m venv ./build-env
-    source ./build-env/bin/activate 
-    ${PYBIN}/pip wheel --wheel-dir=./dist ./swmm-toolkit
+    if [ ${PYBIN} != "/opt/python/cp35-cp35m/bin" ]; then
+        # Setup python virtual environment for build
+        ${PYBIN}/python -m venv --clear ./build-env
+        source ./build-env/bin/activate
+
+        # Install build requirements
+        python -m pip install setuptools wheel scikit-build cmake
+
+        # Build wheel
+        python setup.py bdist_wheel
+        mv ./dist/*.whl ../dist/
+
+        # cleanup
+        python setup.py clean
+        deactivate
+    fi
 done
 
+# Cleanup
+rm -rf ./build-env
+
+
+# Repairing and testing from swmm-python directory
+cd ..
+
 # Bundle external shared libraries into the wheels
-#for whl in wheelhouse/*.whl; do
-#    auditwheel repair $whl -w ./wheelhouse/
-#done
+for whl in ./dist/*-linux_x86_64.whl; do
+    auditwheel repair $whl -w ./dist
+done
+
 
 # Install packages and test
-#for PYBIN in /opt/python/*/bin/; do
-#    ${PYBIN}pip install pytest numpy
-#    ${PYBIN}pip install --no-index --find-links=./dist swmm-toolkit
-#    pytest
-#done
+for PYBIN in /opt/python/*/bin; do
+    if [ ${PYBIN} != "/opt/python/cp35-cp35m/bin" ]; then
+        # Setup python virtual environment for test
+        ${PYBIN}/python -m venv --clear ./test-env
+        source ./test-env/bin/activate
+
+        python -m pip install pytest numpy aenum==2.2.4
+
+        python -m pip install --verbose --no-index --find-links=./dist swmm_toolkit
+        pytest
+
+        deactivate
+    fi
+done
+
+rm -rf ./test-env
