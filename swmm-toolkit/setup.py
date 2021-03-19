d9d4d1f2250a6f5d990e61c4248f73cb5dd9b89e
Michael Tryby <tryby.michael@epa.gov>
2021-03-19 17:41:12 -0400

    Updating classifier

--- swmm-toolkit/setup.py
+++ swmm-toolkit/setup.py
@@ -119,6 +119,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: C",
         "Development Status :: 4 - Beta",
     ]
