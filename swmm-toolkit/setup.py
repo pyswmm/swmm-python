54abe6c742654738755a022c3b6562a5736cb457
karosc <karosc@cdmsmith.com>
2022-02-08 14:41:12 -0500

    update SWMM - update swmm-solver to latest commit - incremenet swmm-python version

--- swmm-toolkit/setup.py
+++ swmm-toolkit/setup.py
@@ -88,7 +88,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name = "swmm-toolkit",
-    version = "0.8.2",
+    version = "0.8.3",
 
     packages = ["swmm_toolkit", "swmm.toolkit"],
     package_dir = package_dir,
