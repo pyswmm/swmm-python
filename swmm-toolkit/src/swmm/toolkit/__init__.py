37ce646890d5f2cc11914df187d4a340a6b1e140
karosc <ckaros@outlook.com>
2023-01-24 20:03:57 -0500

    set release version

--- swmm-toolkit/src/swmm/toolkit/__init__.py
+++ swmm-toolkit/src/swmm/toolkit/__init__.py
@@ -19,7 +19,7 @@ __copyright__ = "None"
 __credits__ = "Colleen Barr, Sam Hatchett"
 __license__ = "CC0 1.0 Universal"
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __date__ = "June 7, 2021"
 
 __maintainer__ = "Michael Tryby"
