119989404c112b2794aec9b9035d0244c2385715
karosc <ckaros@outlook.com>
2023-03-28 10:47:59 -0400

    update OWA SWMM to v5.2.2 and set release version

--- swmm-toolkit/setup.py
+++ swmm-toolkit/setup.py
@@ -88,7 +88,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name = "swmm-toolkit",
-    version = "0.12.0",
+    version = "0.13.0",
 
     packages = ["swmm_toolkit", "swmm.toolkit"],
     package_dir = package_dir,
