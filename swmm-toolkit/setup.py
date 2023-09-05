1899d720a95572f8510fbbb00ec90f75e2cd7de7
karosc <ckaros@outlook.com>
2023-09-05 10:16:37 -0400

    set release version

--- swmm-toolkit/setup.py
+++ swmm-toolkit/setup.py
@@ -107,7 +107,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name = "swmm-toolkit",
-    version = "0.14.2",
+    version = "0.15.0",
 
     packages = ["swmm_toolkit", "swmm.toolkit"],
     package_dir = package_dir,
