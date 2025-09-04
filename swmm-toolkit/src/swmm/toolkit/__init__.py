03a41f329aa10ce48b82d97a7cd309ed99f44a17
Bryant E McDonnell <bemcdonnell@gmail.com>
2025-09-04 09:58:11 -0600

    Staging Release Changes

--- swmm-toolkit/src/swmm/toolkit/__init__.py
+++ swmm-toolkit/src/swmm/toolkit/__init__.py
@@ -4,7 +4,7 @@
 #  __init__.py - SWMM toolkit package
 #
 #  Created:    Aug 9, 2018
-#  Updated:    July 2, 2022
+#  Updated:    September 4, 2025
 #
 #  Author:     See AUTHORS
 #
@@ -15,16 +15,15 @@ A low level pythonic API for the swmm-output and swmm-solver dlls using SWIG.
 
 
 __author__ = "See AUTHORS"
-__copyright__ = "None"
-__credits__ = "Colleen Barr, Sam Hatchett"
+__credits__ = "Colleen Barr"
 __license__ = "CC0 1.0 Universal"
 
-__version__ = "0.15.5"
-__date__ = "June 7, 2021"
+__version__ = "0.16.0"
+__date__ = "September 4, 2025"
 
 __maintainer__ = "Michael Tryby"
-__email__ = "tryby.michael@epa.gov"
-__status__  = "Beta"
+__email__ = "michael.tryby@gmail.com"
+__status__  = "Production/Stable"
 
 
 import os
